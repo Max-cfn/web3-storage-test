d2d015fe6769f369551a5e038c50d3ba56bc50f9
Maxence CERFONTAINE <maxence.cerfontaine@efrei.net>
2022-07-02 15:38:36 +0200

    update

--- store_files.js
+++ store_files.js
@@ -25,13 +25,13 @@ function getFiles() { //obtenir le fichier séléctionné sur la page web par l'
 //make file objects
 function makeFileObjects(fileInput) {
     console.log("in function") //verifier qu'on est dans la fonction.
-    const obj = { hello: 'world' }
-    const blob = new Blob([Text], { type: 'text/plain' })
-
-    const files = [
-        new File(['contents-of-file-1'], 'plain-utf8.txt'),
-        new File([blob], fileInput),
-    ]
+        //const obj = { hello: 'world' }
+        //const blob = new Blob([Text], { type: 'text/plain' })
+    const files = fileInput
+        /*const files = [
+            new File(['contents-of-file-1'], 'plain-utf8.txt'),
+            new File([blob], fileInput),
+        ]*/
     return files
 }
 
